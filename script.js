00e76e45fa929454d6822f369426f6248369792e
Ganapathy77 <ganapathy14777@gmail.com>
2023-03-20 23:17:58 +0530

    Completed Coding

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
+const lookup = {
+  1 : ['1'],
+  2 : ['a','b','c'],
+  3 : ['d', 'e', 'f'],
+  4 : ['g','h','i'],
+  5 : ['j','k','l'],
+  6 : ['m','n','o'],
+  7 : ['p', 'q', 'r', 's'],
+  8 : ['t', 'u', 'v'],
+  9 : ['x', 'y', 'z'],
+  0 : ['0']
+}
 function letterCombinations(input_digit) {
   //Complete the function
+  if(input_digit.length === 0){
+    return[]
+  }
+  let result = [];
+
+  function recur(str, digit){
+    if(digit.length === 0){
+      result.push(str);
+      return result;
+    }
+
+      const letterArr = lookup[digit[0]];
+
+      letterArr.forEach(letter => {
+        recur(str + letter, digit.slice(1))
+      });
+  }
+  
+  recur('', input_digit);
+  return result.sort();
 }
+
+// console.log(letterCombinations("22"))
 
 module.exports = letterCombinations;
